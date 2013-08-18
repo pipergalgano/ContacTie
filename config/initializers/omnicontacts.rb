199a64148c0754117ddcb3f535cdfba660ae9e8e
Piper Galgano <peg48@cornell.edu>
2013-08-18 15:39:14 -0400

    added additional omnicontact file

---
+++ config/initializers/omnicontacts.rb
@@ -0,0 +1,8 @@
+require "omnicontacts"
+
+Rails.application.middleware.use OmniContacts::Builder do
+  importer :gmail, ENV["CONTACT_TIE_GOOGLE_CONTACTS_APP_ID"], ENV["CONTACT_TIE_GOOGLE_CONTACTS_SECRET"]
+  importer :yahoo, "consumer_id", "consumer_secret", {:callback_path => '/callback'}
+  importer :hotmail, "client_id", "client_secret"
+  importer :facebook, "client_id", "client_secret"
+end
